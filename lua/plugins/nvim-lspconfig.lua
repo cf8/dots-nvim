7d9502eb7db367b342d72725b68f44d137af17f3
cf8 <cf8@cf8.ru>
2023-07-16 02:31:34 +0700

    luasnip

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,8 @@
 local plugin = { "neovim/nvim-lspconfig" }
 
 function plugin.config()
-  -- require("lspconfig").stylelint_lsp.setup({})
   require("plugins.lsp.handlers").setup()
+
 end
 
 return plugin
